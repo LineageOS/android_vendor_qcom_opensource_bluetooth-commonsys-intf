a52cfec4882974bcb00c8f7946142d4fb3cef9a5
Bruno Martins <bgcngm@gmail.com>
2021-06-14 12:42:00 +0100

    [DNM][SQUASH] Merge tag 'LA.QSSI.11.0.r1-12200-qssi.0' of https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/bluetooth-commonsys-intf into HEAD
    "LA.QSSI.11.0.r1-12200-qssi.0"
    
        * tag 'LA.QSSI.11.0.r1-12200-qssi.0' of https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/bluetooth-commonsys-intf:
          FR 67742: Vendor Freeze - commonsys-intf interface-only
          Disable QTI BT stack in automotive SP
          Disable SAR for MSM8937 32 bit Go target
          Disable FM in Automotive SP
          Add new enum to read BT maximum power
    
    Change-Id: Id4cb0099fbf867e4b55b79079fa4d33709329072

--- build/qva/config/bdroid_buildcfg.h
+++ build/qva/config/bdroid_buildcfg.h
@@ -1,8 +1,10 @@
 /*
  *  Copyright (c) 2013, The Linux Foundation. All rights reserved.
- *  Not a Contribution.
+ *  Not a Contribution, Apache license notifications and license are retained
+ *  for attribution purposes only.
  *
  * Copyright (C) 2012 The Android Open Source Project
+ * Copyright (C) 2018 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
